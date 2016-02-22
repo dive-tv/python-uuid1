24d7ba5d33167e3c643ebc1dcc921e06ee6d7baf
David Fierro Iglesias <david.fierro@touchvie.com>
2016-02-22 12:18:25 +0100

    Generating the node part of the UUID using the node IP addresses

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@ setup(
   name = 'uuid1',
   packages = ['uuid1'],
   version = '0.1.0',
-  description = 'Pseudo version 1 UUID generator',
+  description = 'Library that generates time sortable UUIDs',
+  long_description = '',
   author = 'David Fierro',
   author_email = 'fierro85@gmail.com',
   url = 'https://github.com/touchvie/python-uuid1',
   download_url = 'https://github.com/touchvie/python-uuid1/tarball/0.1.0',
-  keywords = ['uuid', 'uuid1'],
+  keywords = ['uuid', 'uuid1', 'TimeUUID'],
   classifiers = [],
+  license = 'MIT'
 )
